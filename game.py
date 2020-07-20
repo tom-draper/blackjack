3762dda8b26951017640be401325198026ebb5b8
Tom Draper <tomjdraper1@gmail.com>
2020-07-20 22:22:13 +0100

    Player object

--- game.py
+++ game.py
@@ -2,26 +2,40 @@ import random
 
 
 class Hand():
-    def __init__(self, available):
+    def __init__(self, available_cards):
         self.cards = []
-        self.draw(available)
-        self.draw(available)
-    
-    def draw(self, available):
-        self.cards.append(random.choice(available))
+        self.draw(available_cards)
+        self.draw(available_cards)
+
     
     def __str__(self):
         return str(self.cards)
-        
+
+
+class Player():
+    def __init__(self, available_cards):
+        self.hand = Hand(available_cards)
+    
+    def draw(self, available_cards):
+        self.hand.cards.append(random.choice(available_cards))
+
+
 class Game():
     def __init__(self, no_players=1):
-        self.cards = ['2C', '2D', '2H', '2S', '3C', '3D', '3H', '3S'
-                      '4C', '4D', '4H', '4S', '5C', '5D', '5H', '5S']
+        self.cards = self.refill()
         self.no_players = no_players
-        self.hands = {}
-        self.hands['dealer_hand'] = Hand(self.cards)
+        
+        self.players = {}
+        self.players['dealer'] = Player(self.cards)
         for i in range(self.no_players):
-            self.hands['player{}_hand'.format(i)] = Hand(self.cards)
+            self.players['player{}'.format(i)] = Player(self.cards)
+    
+    def refill(self):
+        return ['2C', '2D', '2H', '2S', '3C', '3D', '3H', '3S'
+                '4C', '4D', '4H', '4S', '5C', '5D', '5H', '5S']
+    
+    def playGame(self):
+        pass
     
     def __str__(self):
         string = '{} players\n'.format(self.no_players)
