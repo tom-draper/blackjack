f369b1d785cfa76c8d66d0c0742e6705562f14df
Tom Draper <tomjdraper1@gmail.com>
2020-07-20 23:25:02 +0100

    Display hand total

--- game.py
+++ game.py
@@ -4,13 +4,27 @@ import random
 class Hand():
     def __init__(self, available_cards):
         self.cards = []
-        self.hand_value = (0)
+        self.hand_value = (0,)  # Tuple containing possible values of this hand
     
     def update_hand_value(self):
         total = 0
         for card in self.cards:
-            total += self.card_value(card)
-        self.hand_value = total
+            card_value = self.card_value(card)
+            
+            new_hand_value = []
+            if type(card_value) is tuple:  # If more than one card value (drawn Ace)
+                # Add each card value to each current hand value
+                # E.g. hand value can be 6 or 16 due to holding an Ace
+                #      new card value could be 1 or 10 (an Ace)
+                #      new hand value would be 7, 16, 17 or 26
+                for hand_value in self.hand_value:
+                    for value in card_value:
+                        new_hand_value.append(hand_value + value)
+            else:
+                for hand_value in self.hand_value:
+                    new_hand_value.append(hand_value + card_value)
+            
+            self.hand_value = tuple(new_hand_value)  # Save result as tuple
     
     def card_value(self, card):
         card_values = {
@@ -22,20 +36,23 @@ class Hand():
             **dict.fromkeys(['7C', '7D', '7H', '7S'], 7),
             **dict.fromkeys(['8C', '8D', '8H', '8S'], 8),
             **dict.fromkeys(['9C', '9D', '9H', '9S'], 9),
-            **dict.fromkeys(['10C', '10D', '3H', '10S'], 10),
-            **dict.fromkeys(['AC', 'AD', 'AH', 'AS'], 11),
-            **dict.fromkeys(['JC', 'JD', 'JH', 'JS'], 10),
-            **dict.fromkeys(['KC', 'KD', 'KH', 'KS'], 10),
-            **dict.fromkeys(['QC', 'QD', 'QH', 'QS'], 10),
+            **dict.fromkeys(['10C', '10D', '3H', '10S',
+                             'JC', 'JD', 'JH', 'JS',
+                             'KC', 'KD', 'KH', 'KS',
+                             'QC', 'QD', 'QH', 'QS'], 10),
         }
-
-        return card_values[card]
+        
+        if card in ['AC', 'AD', 'AH', 'AS']:
+            return (1, 11)
+        else:
+            return card_values[card]
     
     def __str__(self):
         string = ''
         # Print each card in hand
         for card in self.cards:
-            string += str(card) + ' '
+            string += '{} '.format(card)
+        string += ' (Total {})'.format(*self.hand_value)
             
         return string
 
@@ -54,7 +71,7 @@ class Player():
     
     def __str__(self):
         return 'Hand: {}'.format(self.hand)
-
+    
 
 class Game():
     def __init__(self, no_players=1):
@@ -82,13 +99,13 @@ class Game():
     
     def __str__(self):
         # Print dealer
-        string = 'Dealer -- {}\n'.format(self.players['dealer'])
+        string = 'Dealer - {}\n'.format(self.players['dealer'])
         # Print each player
         for i in range(self.no_players):
-            string += 'Player{} -- {}\n'.format(i+1, self.players['player{}'.format(i)])
+            string += 'Player{} - {}\n'.format(i+1, self.players['player{}'.format(i)])
         string += 'Cards remaining: {}'.format(self.cards)
         
         return string
 
-game = Game(3)
+game = Game()
 print(game)
